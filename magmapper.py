b7c5c033d6a540096ff9c6999fc0eb11b2e47d54
sleemanmunk <sleemanmunk@gmail.com>
2010-04-04 16:02:26 -0400

    Added support for automatically detecting sensors and prompting for identification.

--- magmapper.py
+++ magmapper.py
@@ -1,6 +1,6 @@
-import nxt_common as nxtc
+from nxt_common import *
 import record_data_lib
-import hicompass
+import nxt.hicompass as hicompass
 
 import curses as c
 #Some notes about using curses:
@@ -9,6 +9,17 @@ import curses as c
 #and you will return to it when the program is finished.
 #When in curses mode, instead of print, use screen.addstr
 
+
+#Expected Values
+X_NORTH = 0
+X_EAST = 90
+X_SOUTH = 180
+X_WEST = 270
+
+#Ports on NXT for sensors and a key for easy conversion to user-friendly output
+SENSOR_PORTS = [PORT_1,PORT_2,PORT_3,PORT_4]
+PORT_KEY = {PORT_1:"Port 1",PORT_2:"Port 2",PORT_3:"Port 3",PORT_4:"Port 4"}
+
 #Constants for relevant keycodes
 #Further keycodes may be determined with python's ord( char ) function
 UP_ARROW=65
@@ -16,7 +27,7 @@ DOWN_ARROW=66
 SPACEBAR=32
 Q=113
 
-BASE_SPEED=127
+BASE_SPEED=66
 
 FORWARD=1  #This way FORWARD*BASE_SPEED makes the motors go forward
 BACKWARD=-1#and BACKWARD*BASE_SPEED makes the motors go backward
@@ -37,9 +48,25 @@ def stop(bot):
 	return STOPPED
 
 def record_data(bot):
-	pass	
-
+	pass
 #--MAIN--
+
+bot = find_bot()
+
+compasses = []
+compass_ids = []
+
+for port in SENSOR_PORTS:
+	try:
+		compass=hicompass.CompassSensor(bot,port)
+		print "Testing",PORT_KEY[port]
+		if compass.get_type() == "Compass ":#note that there's
+							#an extra space
+			print "Compass Detected in", PORT_KEY[port]
+			compass_ids.append(raw_input("Compass_ID: "))
+			compasses.append(compass)
+	except hicompass.DirProtError:
+		print "Could not communicate"	
 
 screen = c.initscr() #begin the curses environment
 c.noecho()#stop characters auto-echoing to screen
@@ -53,19 +80,21 @@ while not finished:
 	if key == UP_ARROW:
 		if movement != FORWARD:
 			movement = move(bot,FORWARD)
-		else
+		else:
 			movement = stop(bot)
 	elif key == DOWN_ARROW:
 		if movement != BACKWARD:
 			movement = move(bot,BACKWARD)
-		else
+		else:
 			movement = stop(bot)
 	elif key == SPACEBAR:
 		movement = stop(bot)
-		record_data()
+		record_data(bot)
 	elif key == Q:
 		finished = True
 
 c.endwin() #This is critical!
 #If the program exits before this function is called
 #Terrible things will happen
+
+stop_bot(bot)
